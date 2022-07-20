f15e7c8d3aab13a560665168491f923b29367f91
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2022-07-20 13:06:45 +0300

    123

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,29 @@
 import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
-// Add imports above this line
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector('.gallery');
+const galleryAllMarkup = createGalleryAllMarkup(galleryItems);
+
+galleryContainerRef.insertAdjacentHTML('beforeend', galleryAllMarkup);
+
+const modal = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryAllMarkup(galleryItems) {
+  return galleryItems.reduce((acc, { preview, original, description }) => {
+    return (
+      acc +
+      `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" 
+  src="${preview}" 
+  alt="${description}" />
+</a>`
+    );
+  }, '');
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+const player = new Player('handstick', {
+  id: 'vimeo-player',
+  width: 640,
+});
 
+//  require(['https://player.vimeo.com/video/236203659'], function (Player) {
+//         const iframe = document.querySelector('iframe');
+//         const player = new Player(iframe);
+
+// const options = {
+//   id: 'vimeo-player',
+//   width: 640,
+//   loop: true,
+// };
+// const player = new Vimeo.Player('made-in-ny', options);
+
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+
+// const handstickPlayer = new Vimeo.Player('handstick');
+// handstickPlayer.on('play', function () {
+//   console.log('played the handstick video!');
+// });
